6efaf3bd75a2f5600815368e80028ee669016483
KainaAja <ikomangkrisna40@gmail.com>
2024-03-04 10:22:42 +0800

    update

--- app/components/Project.jsx
+++ app/components/Project.jsx
@@ -34,7 +34,7 @@ const Project = () => {
         <section id="project" className="pt-[80px] w-full">
             <div className="bg_project flex flex-col items-center justify-start min-h-fit min-w-fit flex-wrap content-center p-6 mx-12 lg:mx-44 rounded-lg 2xl:mt-[85px]">
                 <h2 className="text-xl font-extrabold text-center bg-[#EDEDED] border-2 border-black p-3 rounded-3xl">My Project</h2>
-                <div className="flex flex-wrap gap-x-[6rem] xl:gap-x-[65px] gap-y-[1rem] xl:gap-y-[0px] pb-6 content-center justify-center items-center">
+                <div className="flex flex-wrap gap-x-[6rem] xl:gap-x-[65px] gap-y-[1rem] xl:gap-y-[0px] pb-6 content-center justify-center items-center">                   
                     {project.map(({ id, attributes }, index) => (
                         <div key={id} className="project-item w-[300px] md:w-[400px]" >
                             <div className="bg-[#adadad] flex-[1 1 30px] p-[16px] rounded-[.5rem] divide-solid relative mt-[3rem] focus:scale-1000 hover:scale-105">
@@ -68,7 +68,7 @@ const Project = () => {
                                 </div>
                             </div>
                         </div>
-                     ))}
+                     ))}               
                 </div>
             </div>
         </section>
